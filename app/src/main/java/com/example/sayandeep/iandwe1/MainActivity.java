6dad79fbd24fc4e9948c090eb732fa4facd288fe
Sayandeep <sayandeepl63@gmail.com>
2018-04-10 19:29:55 +0530

    Percentage Calculation update

--- app/src/main/java/com/example/sayandeep/iandwe1/MainActivity.java
+++ app/src/main/java/com/example/sayandeep/iandwe1/MainActivity.java
@@ -219,7 +219,7 @@ public class MainActivity extends AppCompatActivity {
                 if(i!=-1)
                 {   float n1=Float.parseFloat(exp1.substring(0,i-1));
                     float n2=Float.parseFloat(exp1.substring(i+2,exp1.length()));
-                    float res=(n1/n2)*100;
+                    float res=(n1/100)*n2;
                     etInput.setText(res+"");
                 }
                 else{
